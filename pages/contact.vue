2af9ae49f578edd4776415e72a3e729b3d243c74
tina <tina@ezway-technology.com>
2024-11-11 16:46:11 +0300

    refactor(pages): add index, contact pages

---
+++ pages/contact.vue
@@ -0,0 +1,7 @@
+<script setup>
+</script>
+<template>
+    <div>
+        <h1>Hello Contact</h1>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<script setup>
+</script>
+<template>
+    <div>
+        <h1>Hello Index</h1>
+    </div>
+</template>
