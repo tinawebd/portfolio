56c79b7d7a40267e2df3178e36b0b1b425a7a9bb
tina <tina@ezway-technology.com>
2024-11-13 20:18:42 +0300

    refactor(headings): add component

---
+++ components/atoms/typography/Title.vue
@@ -0,0 +1,17 @@
+<template>
+  <component :is="tag">
+    <slot />
+  </component>
+</template>
+
+<script setup lang="ts">
+export type HeadingTag = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+
+interface Props {
+  tag?: HeadingTag;
+}
+
+withDefaults(defineProps<Props>(), {
+  tag: 'h1',
+});
+</script>
