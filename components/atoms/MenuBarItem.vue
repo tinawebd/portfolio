4b959137e79b2012205059f1a4f3ea2f1eaa50b3
tina <tina@ezway-technology.com>
2024-11-14 21:01:17 +0300

    refactor(menubar): refactor style

---
+++ components/atoms/MenuBarItem.vue
@@ -0,0 +1,11 @@
+<template>
+  <NuxtLink :to="to" class="p-menubar-item-link" data-pc-section="itemLink">
+    {{ label }}
+  </NuxtLink>
+</template>
+<script setup lang="ts">
+defineProps<{
+  to: string
+  label: string
+}>()
+</script>
--- components/molecules/MenuBar.vue
+++ components/molecules/MenuBar.vue
@@ -1,5 +1,26 @@
 <template>
-  <PrimeMenubar :model="items" />
+  <PrimeMenubar :model="items">
+    <template #start>
+      <NuxtLink to="/">
+        <span class="text-2xl font-bold text-primary">Logo</span>
+      </NuxtLink>
+    </template>
+    <template #item="{ item }">
+      <AtomsMenuBarItem :to="item.route" :label="item.label" />
+    </template>
+    <template #end>
+      <div class="flex items-center gap-2">
+        <PrimeAvatar
+          image="https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png"
+          shape="circle"
+        />
+        <PrimeAvatar
+          image="https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png"
+          shape="circle"
+        />
+      </div>
+    </template>
+  </PrimeMenubar>
 </template>
 
 <script setup>
@@ -9,14 +30,17 @@ const items = ref([
   {
     label: 'Home',
     icon: 'pi pi-home',
+    route: '/',
   },
   {
-    label: 'Features',
+    label: 'Realisations',
     icon: 'pi pi-star',
+    route: '/realisations',
   },
   {
     label: 'Contact',
     icon: 'pi pi-envelope',
+    route: '/contact',
   },
 ])
 </script>
