446ec4c05d6c033e705b5da34f0159dab907ac1c
tina <tina@ezway-technology.com>
2024-11-15 17:13:22 +0300

    feat(atoms): add new component svg-icon

---
+++ components/atoms/SvgIcon.vue
@@ -0,0 +1,11 @@
+<template>
+  <svg-icon type="mdi" :path="path" />
+</template>
+
+<script setup lang="ts">
+import SvgIcon from '@jamescoyle/vue-icon'
+
+defineProps<{
+  path: string
+}>()
+</script>
