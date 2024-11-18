3cc4c2145d3d977978b5f0802267caeb9ecc45b7
tina <tina@ezway-technology.com>
2024-11-18 11:15:27 +0300

    build(eslint): refactor config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,16 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default withNuxt({
+  files: ['**/*.ts', '**/*.vue'],
+  rules: {
+    '@typescript-eslint/no-unused-expressions': [
+      'error',
+      {
+        allowShortCircuit: false,
+        allowTernary: false,
+        allowTaggedTemplates: false,
+      },
+    ],
+  },
+})
