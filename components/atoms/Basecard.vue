e335d4bc11ee1bfc020c09b6ba8e97be37acd4c4
tina <tina@ezway-technology.com>
2024-11-19 15:04:51 +0300

    refactor(card): add base card component

---
+++ components/atoms/Basecard.vue
@@ -0,0 +1,21 @@
+<template>
+  <Primecard>
+    <template v-if="$slots.header" #header>
+      <slot name="header"></slot>
+    </template>
+    <template v-if="$slots.title" #title>
+      <slot name="title"></slot>
+    </template>
+    <template v-if="$slots.subtitle" #subtitle>
+      <slot name="subtitle"></slot>
+    </template>
+    <template v-if="$slots.content" #content>
+      <slot name="content"></slot>
+    </template>
+    <template v-if="$slots.footer" #footer>
+      <slot name="footer"></slot>
+    </template>
+  </Primecard>
+</template>
+
+<script setup></script>
