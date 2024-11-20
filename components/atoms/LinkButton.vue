0720cb05124fb0648fd381a6085ba8ba96af2393
tina <tina@ezway-technology.com>
2024-11-20 13:11:28 +0300

    refactor(component): add component LinkButton

---
+++ components/atoms/LinkButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <AtomsButton
+    as="a"
+    :label="label"
+    :href="href"
+    target="_blank"
+    rel="noopener"
+    variant="link"
+    size="large"
+  />
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  label: string;
+  href: string;
+}>();
+</script>
