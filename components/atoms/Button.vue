3fdc65afaceedff604e228769181531253bd5dee
tina <tina@ezway-technology.com>
2024-11-20 13:12:06 +0300

    refactor(Button): add size props

--- components/atoms/Button.vue
+++ components/atoms/Button.vue
@@ -1,5 +1,10 @@
 <template>
-  <PrimeButton :label="label" :severity="severity" :variant="variant" />
+  <PrimeButton
+    :label="label"
+    :severity="severity"
+    :variant="variant"
+    :size="size"
+  />
 </template>
 <script setup lang="ts">
 export type Severity =
@@ -12,13 +17,16 @@ export type Severity =
   | 'danger'
   | 'contrast';
 export type Variant = 'text' | 'outlined' | 'link';
+export type Size = 'small' | 'large';
 interface Props {
   label: string;
   severity?: Severity;
   variant?: Variant;
+  size?: Size;
 }
 withDefaults(defineProps<Props>(), {
-  severity: 'primary',
+  severity: undefined,
   variant: undefined,
+  size: undefined,
 });
 </script>
