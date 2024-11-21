ee6c1a52aa5ec453da29f1b9f3363b541524f5ce
tina <tina@ezway-technology.com>
2024-11-21 13:10:38 +0300

    refactor(Basecard): refactor class and template

--- components/atoms/Basecard.vue
+++ components/atoms/Basecard.vue
@@ -3,9 +3,7 @@
     <template v-if="$slots.header" #header>
       <slot name="header"></slot>
     </template>
-    <template v-if="$slots.title" #title>
-      <slot name="title"></slot>
-    </template>
+    <template v-if="$slots.title" #title><slot name="title"></slot></template>
     <template v-if="$slots.subtitle" #subtitle>
       <slot name="subtitle"></slot>
     </template>
@@ -17,5 +15,3 @@
     </template>
   </Primecard>
 </template>
-
-<script setup></script>
