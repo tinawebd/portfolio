c1e62292a6cb036d291af9aa0175e3c563a72845
tina <tina@ezway-technology.com>
2024-11-25 18:20:56 +0300

    refactor(carousel): refactoring

--- components/organisms/Carousel.vue
+++ components/organisms/Carousel.vue
@@ -4,38 +4,25 @@
     :value="value"
     :num-visible="numVisible"
     :num-scroll="numScroll"
-    :autoplay-interval="autoplayInterval"
-    :circular="circular"
     :orientation="orientation"
   >
     <template #item="slotProps">
-      <slot name="item-template" :item="slotProps.data">
-        <MoleculesRealisationCard
-          :title="slotProps.data.name"
-          :subtitle="slotProps.data.code"
-          :description="slotProps.data.description"
-          :cover-image="slotProps.data.image"
-          :href="slotProps.data.href"
-          class="m-2 w-auto border border-surface-200 dark:border-surface-700"
-        />
-      </slot>
+      <slot name="item-template" :item="slotProps.data"></slot>
     </template>
   </PrimeCarousel>
 </template>
 
 <script setup lang="ts">
 export type Orientation = 'horizontal' | 'vertical';
-interface Props {
+export interface CarouselProps {
   value: Array<object>;
-  numVisible: number;
-  numScroll: number;
-  autoplayInterval?: number;
-  circular?: boolean;
+  numVisible?: number;
+  numScroll?: number;
   orientation?: Orientation;
 }
-withDefaults(defineProps<Props>(), {
-  autoplayInterval: 0,
-  circular: false,
+withDefaults(defineProps<CarouselProps>(), {
   orientation: 'horizontal',
+  numScroll: 1,
+  numVisible: 3,
 });
 </script>
---
+++ components/organisms/VerticalCarousel.vue
@@ -0,0 +1,16 @@
+<template>
+  <OrganismsCarousel
+    :value="value"
+    orientation="vertical"
+    container-class="flex items-center"
+    :num-visible="1"
+  >
+    <template #item-template="{ item }">
+      <slot name="item-vertical-template" :item="item"></slot>
+    </template>
+  </OrganismsCarousel>
+</template>
+<script setup lang="ts">
+import type { CarouselProps } from '../organisms/Carousel.vue';
+defineProps<CarouselProps>();
+</script>
