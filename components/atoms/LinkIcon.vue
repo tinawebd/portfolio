80af2eb87b39bc6ee18e4135e063ccf2e5f5929f
tina <tina@ezway-technology.com>
2024-11-26 17:43:37 +0300

    refactor(components): refactor linkIcon

---
+++ components/atoms/LinkIcon.vue
@@ -0,0 +1,10 @@
+<template>
+  <NuxtLink :to="to"><AtomsSvgIcon :path="icon" /></NuxtLink>
+</template>
+<script setup lang="ts">
+interface LinkIconProps {
+  to: string;
+  icon: string;
+}
+defineProps<LinkIconProps>();
+</script>
---
+++ components/molecules/SocialNetworks.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex items-center gap-2">
+    <AtomsLinkIcon :to="linkedin" :icon="mdiLinkedin" />
+    <AtomsLinkIcon :to="email" :icon="mdiEmailOutline" />
+    <AtomsLinkIcon :to="github" :icon="mdiGithub" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { mdiLinkedin, mdiEmailOutline, mdiGithub } from '@mdi/js';
+const linkedin = ref('https://www.linkedin.com/in/tina-fetraniaina-685068185/');
+const email = ref('mailto:someone@example.com');
+const github = ref('https://github.com/TinaFetraniaina');
+</script>
