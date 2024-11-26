c7bc60bd9d8f7dc10430c77f8ca5980ec0fb5bd7
tina <tina@ezway-technology.com>
2024-11-26 17:44:19 +0300

    refactor: refactor footer

--- components/organisms/Footer.vue
+++ components/organisms/Footer.vue
@@ -2,7 +2,7 @@
   <footer
     class="flex items-center justify-center gap-4 bg-surface-900 p-4 text-surface-200"
   >
-    <p>Intégration et développement - Tina</p>
+    <p>Conçu avec ❤️ et VueJS - Intégration et développement - Tina</p>
   </footer>
 </template>
 <script setup lang="ts"></script>
