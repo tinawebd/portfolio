178c35b9184fc1fbf7d934aff1b6c6a7a238c066
tina <tina@ezway-technology.com>
2024-11-27 17:14:57 +0300

    refactor(component): sectionCard

---
+++ components/organisms/SectionCard.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="my-8 border-t-2 border-surface-200 dark:border-surface-900">
+    <h6 class="my-4 capitalize text-primary">{{ title }}</h6>
+    <h4 v-show="subtitle" class="my-4 capitalize text-surface-50">
+      {{ subtitle }}
+    </h4>
+    <slot name="content"></slot>
+  </section>
+</template>
+
+<script lang="ts" setup>
+import type { CardProps } from '../atoms/Card.vue';
+defineProps<CardProps>();
+</script>
