a10da7132f9a55161a3963698b40f93d742718f8
tina <tina@ezway-technology.com>
2024-11-27 17:13:25 +0300

    refactor(page): add page about

---
+++ components/templates/About.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="mx-auto max-w-screen-xl py-8">
+    <OrganismsSectionCard
+      title="à propos de moi"
+      subtitle="qui suis-je ?"
+      class="border-t-0"
+    >
+      <template #content
+        ><AtomsTextDescription :description="description"
+      /></template>
+    </OrganismsSectionCard>
+    <OrganismsSectionCard title="Mode de travail" subtitle="Indépendant">
+      <template #content
+        ><AtomsTextDescription :description="description"
+      /></template>
+    </OrganismsSectionCard>
+    <OrganismsSectionCard
+      title="Réseaux et Contacts"
+      subtitle="Contactez-moi & Explorez Mon Travail"
+    >
+      <template #content><OrganismsContact /></template>
+    </OrganismsSectionCard>
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  title: string;
+  description: string;
+}>();
+</script>
--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,9 @@
 <script setup></script>
 <template>
-  <div>
-    <h1>Hello About</h1>
-  </div>
+  <TemplatesAbout :title="title" :description="description" />
 </template>
+<script setup lang="ts">
+const title = 'Qui suis-je ?';
+const description =
+  'Développeur web avec 4 ans d’expérience, actuellement focalisé sur Laravel et Vue.js pour concevoir des applications modernes et performantes. Au fil de mon parcours, j’ai travaillé avec diverses technologies telles que Symfony, Angular, Drupal, jQuery, WordPress, Docker, et Bootstrap, enrichissant ainsi mes compétences techniques.';
+</script>
