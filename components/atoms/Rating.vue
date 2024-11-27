8d9efeb302bf8f43bc7cf7d0bcca423457b78b6a
tina <tina@ezway-technology.com>
2024-11-27 13:18:54 +0300

    feat(component): rating component

---
+++ components/atoms/Rating.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="card flex justify-center">
+    <PrimeRating v-model="localValue" readonly />
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  value: number;
+}>();
+const localValue = ref(props.value);
+</script>
---
+++ components/molecules/TechnologieRating.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex items-center gap-2 border-2 border-surface-200 p-2">
+    <AtomsSvgIcon :path="path" />
+    <AtomsRating :value="value" class="text-primary" />
+  </div>
+</template>
+<script setup lang="ts">
+import type { SvgIconProps } from '../atoms/SvgIcon.vue';
+defineProps<{
+  path: SvgIconProps['path'];
+  value: number;
+}>();
+</script>
