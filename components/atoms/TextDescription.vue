6692eafb6102776bce08dd8794d9e5a20deb99c6
tina <tina@ezway-technology.com>
2024-11-27 17:14:11 +0300

    refactor(component): new component TextDescription

---
+++ components/atoms/TextDescription.vue
@@ -0,0 +1,10 @@
+<template>
+  <p class="mx-auto max-w-screen-md p-4 text-surface-400">
+    {{ description }}
+  </p>
+</template>
+<script setup lang="ts">
+defineProps<{
+  description: string;
+}>();
+</script>
